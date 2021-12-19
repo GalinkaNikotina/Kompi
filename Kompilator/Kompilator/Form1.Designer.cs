3cc145a8bb511eff4419d755a4daa8d57cdf8eb4
Galinka <galinka2002niko@gmail.com>
2021-12-20 00:13:51 +0300

    mn

---
+++ Kompilator/Kompilator/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Kompilator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FrameForkTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.stausBox = new System.Windows.Forms.RichTextBox();
+            this.codeBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выходной файл";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(381, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FrameWork";
+            // 
+            // FrameForkTextBox
+            // 
+            this.FrameForkTextBox.Location = new System.Drawing.Point(468, 45);
+            this.FrameForkTextBox.Name = "FrameForkTextBox";
+            this.FrameForkTextBox.Size = new System.Drawing.Size(147, 22);
+            this.FrameForkTextBox.TabIndex = 2;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(156, 45);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(188, 22);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // stausBox
+            // 
+            this.stausBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.stausBox.Location = new System.Drawing.Point(41, 329);
+            this.stausBox.Name = "stausBox";
+            this.stausBox.Size = new System.Drawing.Size(530, 96);
+            this.stausBox.TabIndex = 4;
+            this.stausBox.Text = "";
+            // 
+            // codeBox
+            // 
+            this.codeBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.codeBox.Location = new System.Drawing.Point(41, 111);
+            this.codeBox.Name = "codeBox";
+            this.codeBox.Size = new System.Drawing.Size(540, 128);
+            this.codeBox.TabIndex = 5;
+            this.codeBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(474, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.codeBox);
+            this.Controls.Add(this.stausBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.FrameForkTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CSCompiler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FrameForkTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.RichTextBox stausBox;
+        private System.Windows.Forms.RichTextBox codeBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Kompilator/Kompilator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using Microsoft.CSharp;
+
+namespace Kompilator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CSharpCodeProvider provider = new CSharpCodeProvider(new Dictionary<string, string>() { {"CompilerVersion", FrameForkTextBox.Text } });
+
+            CompilerParameters parameters = new CompilerParameters(new string[] { "mscorlib.dll", "System.Core.dll" }, nameTextBox.Text, true);
+
+            parameters.GenerateExecutable = true;
+
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, codeBox.Text);
+
+            if (results.Errors.HasErrors)
+            {
+                foreach(CompilerError error in results.Errors.Cast<CompilerError>())
+                {
+                    stausBox.Text += $"Строка {error.Line}:  {error.ErrorText}";
+                }
+            }
+            else
+            {
+                stausBox.Text = "Сборка значений";
+
+                Process.Start($"{Application.StartupPath}/{nameTextBox.Text}");
+            }
+        }
+    }
+}
